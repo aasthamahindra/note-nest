f2f314dea7976534c24b705079162098ad698cc6
Aastha Mahindra <155626319+aasthamahindra@users.noreply.github.com>
2025-07-14 13:18:14 +0530

    routes - notes (CRUD)

---
+++ backend/src/routes/note.routes.js
@@ -0,0 +1,8 @@
+const { getAllNotes, createNote, updateNote, deleteNote} = require('../controllers/note.controller');
+
+module.exports = (fastify) => {
+    fastify.get('/', getAllNotes);
+    fastify.post('/', createNote);
+    fastify.put('/:id', updateNote);
+    fastify.deleteNote('/:id', deleteNote);
+};
