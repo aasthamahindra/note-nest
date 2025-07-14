f03ecd354749da6488f994893dc07aca0cfa9d77
Aastha Mahindra <155626319+aasthamahindra@users.noreply.github.com>
2025-07-14 11:55:48 +0530

    added model for mongodb

---
+++ backend/src/models/note.model.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require('mongoose');
+
+const noteSchema = new Schema({
+    title: { type: String, required: true },
+    content: { type: String, require: true },
+    category: {
+        type: String,
+        enum: ['Wishlist', 'Assignment', 'Projects', 'Work', 'Study', 'Others'],
+        required: true,
+    },
+    time: { type: String, required: true },
+    date: { type: String, required: true },
+},
+{ timestamps: true });
+
+const Notes = model('notes', noteSchema, 'notes')
+
+module.exports = { Notes };
